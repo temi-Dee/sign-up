a550d8a254840200e877cc30534f5ac55b7d10a7
temi-Dee <ayotemi007@gmail.com>
2022-06-01 14:26:31 +0000

    my first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+functionAddNew() {
+    const firstname = document.getElementById("fname").value;
+    const lastname = document.getElementById("lname").value
+
+    const fullname = ("firstname" + " " + "lastname");
+    const user = [];
+    console.log(fullname);
+
+}
+
+console.log(functionAddNew);
