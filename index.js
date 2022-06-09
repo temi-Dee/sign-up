c9a1f94f52bb2405960d102d8d5103ede49270b6
temi-Dee <ayotemi007@gmail.com>
2022-06-09 07:46:58 +0100

    banner-text was changed and label under form was also changed under the submit.html

--- index.js
+++ index.js
@@ -5,3 +5,18 @@ function display() {
 
 
 }
+
+
+function readTextFile(file) {
+    const rawFile = new XMLHttpRequest();
+    rawFile.open("GET", file, false);
+    rawFile.onreadystatechange = function() {
+        if (rawFile.readyState === 4) {
+            if (rawFile.status === 200 || rawFile.status == 0) {
+                var allText = rawFile.responseText;
+                alert(allText);
+            }
+        }
+    }
+    rawFile.send(null);
+}
